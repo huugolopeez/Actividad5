a341509d9617ffb6d546e0f00207bb4f789fac48
hugo <huugolopeez024@gmail.com>
2024-03-01 14:11:33 +0100

    commit ejercicio2

---
+++ app/src/main/java/com/hugo/actividad5/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hugo.actividad5
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layaout)
+    }
+}
