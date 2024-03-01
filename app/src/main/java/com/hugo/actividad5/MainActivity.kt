486626c020844b15a4d43eb37c24bb41ccfc0c12
hugo <huugolopeez024@gmail.com>
2024-03-01 14:13:41 +0100

    commit ejercicio4

--- app/src/main/java/com/hugo/actividad5/MainActivity.kt
+++ app/src/main/java/com/hugo/actividad5/MainActivity.kt
@@ -14,5 +14,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layaout)
+
+        val buttonChangeColor = findViewById<Button>(R.id.buttonChangeColor)
+        val buttonSwitchActivity = findViewById<Button>(R.id.buttonSwitchActivity)
+
+        buttonChangeColor.setOnClickListener {
+            // Cambiar el color de fondo del cuerpo
+            val newColor = Color.rgb(0, 0, 255)
+            val container = findViewById<LinearLayout>(R.id.container)
+            container.setBackgroundColor(newColor)
+        }
+
+        buttonSwitchActivity.setOnClickListener {
+            // Cambiar a Activity 2
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        }
     }
 }
