e2b76ab5002b933fb9c9bca10d403b0315d36efc
hugo <huugolopeez024@gmail.com>
2024-03-01 14:47:04 +0100

    commit ejercicio5:  -Activity2: he almacenado el mensaje que recibo de MainActivity y lo muestro en el textView que he recibido antes

--- app/src/main/java/com/hugo/actividad5/Activity2.kt
+++ app/src/main/java/com/hugo/actividad5/Activity2.kt
@@ -11,8 +11,14 @@ class Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2_layaout)
 
-        val textView = findViewById<TextView>(R.id.textView)
         val typeface = Typeface.createFromAsset(assets, "fonts/comic.ttf")
+        val message = intent.getStringExtra("message")
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
+
+        textViewMessage.text = message
         textView.typeface = typeface
+        textViewMessage.typeface = typeface
     }
 }
